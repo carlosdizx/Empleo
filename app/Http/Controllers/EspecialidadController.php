66cb5f716521fdc8bfd70dc17fe6d065a8d4a6d0
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-13 16:25:07 -0500

    Construida las rutas  y vistas para egresado y especialidad

--- app/Http/Controllers/EspecialidadController.php
+++ app/Http/Controllers/EspecialidadController.php
@@ -14,7 +14,7 @@ class EspecialidadController extends Controller
      */
     public function index()
     {
-        //
+        return view("especialidad.index");
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EgresadoController;
+use App\Http\Controllers\EspecialidadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::resource('egresado',EgresadoController::class);
+
+Route::resource('especialidad',EspecialidadController::class);
