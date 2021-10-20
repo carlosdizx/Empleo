84981ee305148c802e3eb0d64e6ace6e0ad9e3cc
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-20 16:08:52 -0500

    Construida una tabla que permite listar las especialidades

--- resources/views/especialidad/index.blade.php
+++ resources/views/especialidad/index.blade.php
@@ -2,10 +2,31 @@
 @section('title','Página inicial')
 @section('content')
     <div class="row g-2">
+        <h1>Listado de especialidades</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
         @foreach($especialidades as $especialidad)
-            <ul class="list-group">
-                <li class="list-group-item">{{$especialidad->nombre}}</li>
-            </ul>
+            <tr>
+                <td>{{$especialidad->id}}</td>
+                <td>{{$especialidad->nombre}}</td>
+                <td>
+                    <a class="btn btn-warning">
+                        <i class="fa fa-pencil"></i>
+                    </a>
+                    <a class="btn btn-danger">
+                        <i class="fa fa-trash-alt"></i>
+                    </a>
+                </td>
+            </tr>
         @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,6 +11,9 @@
             integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
             crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+          integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--
     <link rel="stylesheet" href="assets/sa2/dist/sweetalert2.min.css">
     -->
