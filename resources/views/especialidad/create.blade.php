a698b45334c9629c209f29710fd98951cba250b7
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-20 16:43:38 -0500

    Mejorando el token

--- resources/views/especialidad/create.blade.php
+++ resources/views/especialidad/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.template')
 @section('title','Página inicial')
 @section('content')
-    <form action="{{ url('/especialidad') }}" method="post" enctype="multipart/form-data" autocomplete="off">
+    <form action="{{ url('/especialidad') }}" method="post" autocomplete="off">
         @csrf
         <input type="text" class="form-control" name="nombre" placeholder="Nombre" required>
         <br />
